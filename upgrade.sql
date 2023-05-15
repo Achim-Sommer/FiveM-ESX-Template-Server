d14bb250bd8485ca6ca541e3fff8c825646e829b
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-15 19:49:41 +0200

    Create upgrade.sql

---
+++ upgrade.sql
@@ -0,0 +1,16 @@
+-- --------------------------------------------------------
+----Upgrade SQL von Template Server 4.0
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `multicharacter_slots`
+--
+
+CREATE TABLE `multicharacter_slots` (
+	`identifier` VARCHAR(60) NOT NULL,
+	`slots` INT(11) NOT NULL,
+	PRIMARY KEY (`identifier`) USING BTREE,
+	INDEX `slots` (`slots`) USING BTREE
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
