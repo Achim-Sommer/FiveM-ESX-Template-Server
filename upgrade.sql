62402e86c222fb5e7b57f5bf67544be81c98da47
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-15 20:11:14 +0200

    add table user

--- upgrade.sql
+++ upgrade.sql
@@ -14,3 +14,10 @@ CREATE TABLE `multicharacter_slots` (
 ) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
+
+
+ALTER TABLE `users` ADD COLUMN `metadata` LONGTEXT NULL DEFAULT NULL;
+ALTER TABLE `users` ADD COLUMN `id` int(11) NOT NULL;
+ALTER TABLE `users` ADD COLUMN `disabled` TINYINT(1) NULL DEFAULT '0';
+ALTER TABLE `users` ADD COLUMN `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `users` ADD COLUMN `last_seen` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
