92f068c0bb4f6b87df5955b5be62635e625476f9
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-17 01:38:43 +0200

    resource to fxmanifest

---
+++ [extras]/Peds/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+description 'Peds Script by [SF.L] Master'
+version '1.5.2'
+
+
+-client_scripts {
+
+	'client/main.lua',
+	'config.lua'
+}
