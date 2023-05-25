a7891ca48a78baa007407e01faa4b16b258f4b02
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-25 14:57:17 +0200

    add esx status

---
+++ [esxaddon]/esx_status/esx_status.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `users` ADD COLUMN `status` LONGTEXT NULL;
---
+++ [esxaddon]/esx_status/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Status'
+
+version '1.0'
+legacyversion '1.9.1'
+
+lua54 'yes'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/classes/status.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/scripts/app.js'
+}
+
+dependency 'es_extended'
