ef2e4a1797da6afce027dba1ead5101102eb0365
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-26 20:41:46 +0200

    fix

--- [esxjobs]/esx_vehicleshop/config.lua
+++ [esxjobs]/esx_vehicleshop/config.lua
@@ -1,5 +1,5 @@
 Config                            = {}
-Config.DrawDistance               = 10
+Config.DrawDistance               = 20
 Config.MarkerColor                = {r = 120, g = 120, b = 240}
 Config.EnablePlayerManagement     = false -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
 Config.ResellPercentage           = 50
