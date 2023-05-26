c2fa7d21713cbb3d2cd4e952517b9cb3164175d7
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-26 14:58:58 +0200

    update

--- server.sql
+++ server.sql
@@ -2139,3 +2139,11 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=31
 ;
+
+CREATE TABLE IF NOT EXISTS `pickle_prisons` (
+  `identifier` varchar(46) NOT NULL,
+  `prison` varchar(50) DEFAULT 'default',
+  `time` int(11) NOT NULL DEFAULT 0,
+  `inventory` longtext NOT NULL,
+  `sentence_date` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
--- upgrade.sql
+++ upgrade.sql
@@ -417,3 +417,11 @@ INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 	('rope', 'Seil', 1),
 	('shovel', 'Schaufel', 1)
 ;
+
+CREATE TABLE IF NOT EXISTS `pickle_prisons` (
+  `identifier` varchar(46) NOT NULL,
+  `prison` varchar(50) DEFAULT 'default',
+  `time` int(11) NOT NULL DEFAULT 0,
+  `inventory` longtext NOT NULL,
+  `sentence_date` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
