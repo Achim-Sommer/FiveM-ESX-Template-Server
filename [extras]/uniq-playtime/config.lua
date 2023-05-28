022127cce8cbea36b853db139e2447b2214001a2
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-05-28 20:03:25 +0200

    übersetzung

--- [extras]/uniq-playtime/config.lua
+++ [extras]/uniq-playtime/config.lua
@@ -3,13 +3,13 @@ Shared = {
         mytime = 'mytime',
         timelist = 'timelist'
     },
-    topList = 50, -- how many players will be shown in leaderboard list, for me going over 50 crashed me few times,
+    topList = 10, -- how many players will be shown in leaderboard list, for me going over 50 crashed me few times,
     print = true, -- if you dont want server prints for people after they leave disable this
     Locales = {
-        [1] = 'You played on this server for: %s days %s hours %s min %s sec', 
-        [2] = '%s days %s hours %s min %s sec',
-        [3] = 'Your Play Time',
-        [4] = '%s played on server for %.2f sec',
+        [1] = 'Du hast auf diesem Server gespielt für: %s Tage %s Stunden %s min %s sec', 
+        [2] = '%s Tage %s Stunden %s min %s sec',
+        [3] = 'Deine Spielzeit',
+        [4] = '%s spielte auf dem Server für %.2f sec',
         [5] = 'Top %s',
     }
 }
