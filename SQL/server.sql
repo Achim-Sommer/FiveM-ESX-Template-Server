93d73bbcf1128e96b8802133bb5fb6655af0a419
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-06-27 17:42:54 +0200

    Update server.sql

--- SQL/server.sql
+++ SQL/server.sql
@@ -1855,7 +1855,7 @@ INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_
 	('banker',1,'banker','Banker',20,'{}','{}'),
 	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
 	('banker',3,'trader','Broker',40,'{}','{}'),
-	('banker',4,'boss','Boss',0,'{}','{}'),
+	('banker',4,'boss','Boss',0,'{}','{}')
 ;
 
 
