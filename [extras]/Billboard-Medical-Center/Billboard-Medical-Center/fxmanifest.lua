eb739f110ff46dc3a8f2e97a9d35f51637bda189
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2023-06-30 04:35:37 +0200

    Custom Animated Billboard Pillbox Medical Center

---
+++ [extras]/Billboard-Medical-Center/Billboard-Medical-Center/fxmanifest.lua
@@ -0,0 +1,15 @@
+lua54 'yes'
+
+fx_version 'bodacious'
+game 'gta5'
+
+author '70K3R'
+description 'CUSTOMMADE BILLBOARD MEDICAL CENTER'
+
+this_is_a_map 'yes'
+
+files {
+      'stream/billboard_medical_center.ydr',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'billboard_medical_center.ytyp'
